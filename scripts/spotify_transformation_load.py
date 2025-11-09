151d3276b88c76438ceaa3301c68d4278636621d
erb3erb3erb3 <49728807+erb3erb3erb3@users.noreply.github.com>
2025-11-09 16:47:11 -0500

    Refactor data processing functions with comments
    Refactor functions to improve clarity and maintainability. Added comments to explain data parsing and processing steps.

--- scripts/spotify_transformation_load.py
+++ scripts/spotify_transformation_load.py
@@ -6,6 +6,9 @@ from io import StringIO
 
 
 def album(data):
+
+    # Data is jsonObject, in dict format.
+    # Parses out relevant album information and stores as a dict in album_list array
     album_list = []
     for row in data['items']:
         album_id = row['track']['album']['id']
@@ -13,24 +16,38 @@ def album(data):
         album_release_date = row['track']['album']['release_date']
         album_total_tracks = row['track']['album']['total_tracks']
         album_url = row['track']['album']['external_urls']['spotify']
-        album_element = {'album_id': album_id, 'name': album_name, 'release_date': album_release_date, 'total_tracks': album_total_tracks, 
-            'url': album_url}
+
+        # Create album_element as a dict containing Album information and append to the album_list array
+        album_element = {'album_id': album_id, 'name': album_name, 'release_date': album_release_date, 
+                         'total_tracks': album_total_tracks, 'url': album_url}
         album_list.append(album_element)
 
     return album_list
 
 def artist(data):
+
+    # Data is jsonObject, in dict format.
+    # Parses out relevant artist information and stores as a dict in artist_list array
     artist_list = []
     for row in data['items']:
         for key, value in row.items():
             if key == "track":
+                # To handle case of multiple artists for a single track, iterates through each 'artists' value
                 for artist in value['artists']:
-                    print(artist)
-                    artist_element = {'artist_id': artist['id'], 'artist_name': artist['name'], 'artist_url': artist['external_urls']['spotify']}
+                    artist_id = artist['id']
+                    artist_name = artist['name']
+                    artist_url = artist['eternal_urls']['spotify']
+                    # Create artist_element as a dict containing Artist information and append to the artist_list array
+                    artist_element = {'artist_id': artist_id, 'artist_name': artist_name, 
+                                      'artist_url': artist_url}
                     artist_list.append(artist_element)
+                    
     return artist_list
 
 def song(data):
+    
+    # Data is jsonObject, in dict format.
+    # Parses out relevant song information and stores as a dict in song_list array
     song_list = []
     for row in data['items']:
         #id name duration url popularity added album_id artist_id
@@ -41,7 +58,11 @@ def song(data):
         song_popularity = row['track']['popularity']
         song_added = row['added_at']
         album_id = row['track']['album']['id']
+        # Stores only the first, Primary Artist ID
+        # Could add multiple artist fields, and coalesce with NULL in future
         artist_id = row['track']['artists'][0]['id']
+
+        # Create song_element as a dict containing Song information and append to the song_list array
         song_element = {'song_id': song_id, 'song_name': song_name, 'duration_ms': song_duration, 'url': song_url, 'popularity': song_popularity,
                         'song_added': song_added, 'album_id': album_id, 'artist_id': artist_id}
         song_list.append(song_element)
@@ -50,17 +71,20 @@ def song(data):
 
 
 def lambda_handler(event, context):
+    # Initiate boto3 to talk with S3
+    # Define Bucket and Key we are working with
     s3 = boto3.client('s3')
     Bucket=os.environ.get('gen-bucket')
     Key=os.environ.get('gen-key')
     spotify_data = []
     spotify_keys = []
 
-    ## Loop through files in the s3 bucket to be read.
+    # Loop through files in the s3 bucket to be read.
     for file in s3.list_objects(Bucket=Bucket, Prefix=Key)['Contents']:
         file_key = file['Key']
 
-        ## Check if it is a json file. Reads the file. 
+        # Check if it is a json file. Reads the file if it is. 
+        # Uses json.loads to read json and store it as a dict inside the spotify_data array
         if file_key.split('.')[-1] == 'json':
             response = s3.get_object(Bucket = Bucket, Key = file_key)
             content = response['Body']
@@ -69,10 +93,12 @@ def lambda_handler(event, context):
             spotify_keys.append(file_key)
 
     for data in spotify_data:
+        # Calls functions to extract respective data from our jsonObject(s)
         album_list = album(data)
         artist_list = artist(data)
         song_list = song(data)
 
+        # Create Album, Artist, and Song Dataframes and drops respective duplicates.
         album_df = pd.DataFrame.from_dict(album_list)
         album_df = album_df.drop_duplicates(subset=['album_id'])
 
@@ -104,7 +130,8 @@ def lambda_handler(event, context):
         song_df.to_csv(song_buffer, sep='|', index = False, escapechar='\\')
         song_content = song_buffer.getvalue()
         s3.put_object(Bucket=Bucket, Key=song_key, Body=song_content)
-    
+
+    # Uses s3 resource to clean up S3 folders
     s3_resource = boto3.resource('s3')
     for key in spotify_keys:
         copy_source = {
